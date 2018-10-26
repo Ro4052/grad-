442705ddb5b2b6e4cbcc83725b22577ca65a92e1
Ronan Graham <Ro4052@users.noreply.github.com>
2018-10-26 09:07:36 +0100

    Setup basic display for books

--- src/main/client/src/App.js
+++ src/main/client/src/App.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      books: []
+    };
+    this.getBooks();
+  }
+
+  getBooks() {
+    axios.get('/api/books').then((res) => {
+      this.setState({
+        books: res.data
+      });
+    });
+  }
+
   render() {
     return (
       <div>
         <h1> Grad Library App </h1>
+        <ul>
+          {this.state.books.map((book) =>
+            <li key={book.id}>
+              <h3> {book.title} </h3>
+              <p>
+                id: {book.id},
+                author: {book.author},
+                isbn: {book.isbn},
+                publishDate: {book.publishDate}
+              </p>
+            </li>
+          )}
+        </ul>
       </div>
     );
   }
