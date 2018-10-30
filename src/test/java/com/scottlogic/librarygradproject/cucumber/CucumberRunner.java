52e9db9950fe896025c5ed4a76f5f84da499985f
David Withers <dwithers@scottlogic.co.uk>
2018-10-30 11:29:59 +0000

    Testing if acceptance test (placeholder) run by circleci

---
+++ src/test/java/com/scottlogic/librarygradproject/cucumber/CucumberRunner.java
@@ -0,0 +1,15 @@
+package com.scottlogic.librarygradproject.cucumber;
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        glue = {"src/test/java/com/scottlogic/librarygradproject/cucumber/steps"},
+        features = {"src/test/java/com/scottlogic/librarygradproject/cucumber/features"}
+)
+
+public class CucumberRunner {
+}
