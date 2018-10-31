02f69db4051bdc0949cd01fcc485ba04bb6a49ba
David Withers <dwithers@scottlogic.co.uk>
2018-10-31 09:55:53 +0000

    Front end functional but untested, back-end still in progress, no validation

--- src/main/client/src/App.js
+++ src/main/client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
 import styles from './App.module.css';
 import BookList from './bookList/BookList';
+import AddBook from './addBook/AddBook';
 
 class App extends Component {
 	render() {
@@ -10,6 +11,7 @@ class App extends Component {
 			<div>
 				<h1 className={styles.pageHeader}> Grad Library App </h1>
 				<BookList books={this.props.books}></BookList>
+				<AddBook></AddBook>
 			</div>
 		);
 	}
