cd3afde5f7abb1026a49c7a82b3f8467ce684279
EllieHield <42339621+EllieHield@users.noreply.github.com>
2018-11-01 13:42:14 +0000

    Finished front end functionality and tests

--- src/main/client/src/__tests__/bookList/book/book.test.js
+++ src/main/client/src/__tests__/bookList/book/book.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Book from "../../../bookList/book/Book";
 import { shallow } from "enzyme";
 
@@ -13,12 +12,16 @@ const testBook = {
 
 describe('Book Validation Tests', () => {
       test('title matches test data', () => {
+            // const store = mockStore(initialState);
             const wrapper = shallow(<Book book={testBook}></Book>)
+            // const component = wrapper.dive()
             expect(wrapper.find("h3").text()).toBe(testBook.title)
       })
 
       test('all fields are populated', () => {
+            // const store = mockStore(initialState);
             const wrapper = shallow(<Book book={testBook}></Book>)
+            // const component = wrapper.dive()
             expect(wrapper.find("p").text()).toBe(
                   `id: ${testBook.id}, `
                   + `author: ${testBook.author}, `
