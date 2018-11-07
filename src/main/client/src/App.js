5b199e96f642cf505c911d29994fd2f5b9f742ed
Ro4052 <Ro4052@users.noreply.github.com>
2018-11-07 11:33:42 +0000

    Added eslint and prettier for vscode plugins

--- src/main/client/src/App.js
+++ src/main/client/src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import styles from './App.module.css';
-import BookList from './bookList/BookList';
-import AddBook from './addBook/AddBook';
+import styles from "./App.module.css";
+import BookList from "./bookList/BookList";
+import AddBook from "./addBook/AddBook";
 
 export default class App extends Component {
-	render() {
-		return (
-			<div>
-				<h1 className={styles.pageHeader}> Grad Library App </h1>
-				<BookList></BookList>
-				<AddBook></AddBook>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <h1 className={styles.pageHeader}> Grad Library App </h1>
+        <BookList />
+        <AddBook />
+      </div>
+    );
+  }
 }
