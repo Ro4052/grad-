f34101ade62cb84c1c7a64721cdb400575832fdd
EllieHield <42339621+EllieHield@users.noreply.github.com>
2018-11-09 16:41:18 +0000

    added LibraryUser class to create library_user table in database

---
+++ src/main/java/com/scottlogic/librarygradproject/LibraryUser.java
@@ -0,0 +1,19 @@
+package com.scottlogic.librarygradproject;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class LibraryUser {
+    @Id
+    private String username;
+
+    public LibraryUser() { }
+
+    public LibraryUser(String username) {
+        this.username = username;
+    }
+}
