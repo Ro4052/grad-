fd57c03bf098af10ee7226e667c0aab04bbf273c
Ronan Graham <Ro4052@users.noreply.github.com>
2018-11-12 11:25:01 +0000

    Toggle between filed ellipsis and breaking on click

--- src/main/client/src/dashBoard/bookList/book/Book.js
+++ src/main/client/src/dashBoard/bookList/book/Book.js
@@ -3,6 +3,28 @@ import EditBook from "./EditBook";
 import styles from "./Book.module.css";
 
 export default class Book extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullTitle: false,
+      fullAuthor: false
+    };
+    this.toggleTitle = this.toggleTitle.bind(this);
+    this.toggleAuthor = this.toggleAuthor.bind(this);
+  }
+
+  toggleTitle() {
+    this.setState({
+      fullTitle: !this.state.fullTitle
+    });
+  }
+
+  toggleAuthor() {
+    this.setState({
+      fullAuthor: !this.state.fullAuthor
+    });
+  }
+
   render() {
     const { book } = this.props;
     return (
@@ -19,13 +41,29 @@ export default class Book extends Component {
             <label htmlFor={book.id}>{book.title}</label>
           </h3>
         ) : (
-          <h3 id={`bookTitle${book.id}`} className={styles.bookField}>
+          <h3
+            id={`bookTitle${book.id}`}
+            className={
+              this.state.fullTitle
+                ? styles.bookFieldBreak
+                : styles.bookFieldEllipsis
+            }
+            onClick={this.toggleTitle}
+          >
             {book.title}
           </h3>
         )}
         <div className={styles.bookDetails}>
           <div id={`isbn${book.id}`}>ISBN: {book.isbn}</div>
-          <div id={`author${book.id}`} className={styles.bookField}>
+          <div
+            id={`author${book.id}`}
+            className={
+              this.state.fullAuthor
+                ? styles.bookFieldBreak
+                : styles.bookFieldEllipsis
+            }
+            onClick={this.toggleAuthor}
+          >
             Author: {book.author}
           </div>
           <div id={`publishDate${book.id}`}>
