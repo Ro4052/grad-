61e3e91fec9ac54d942beb1d95c577de2a8c5011
EllieHield <42339621+EllieHield@users.noreply.github.com>
2018-11-20 16:57:33 +0000

    added loan not found exception, delete, find one, find all

--- src/main/java/com/scottlogic/librarygradproject/Controllers/BorrowController.java
+++ src/main/java/com/scottlogic/librarygradproject/Controllers/BorrowController.java
@@ -1,5 +1,6 @@
 package com.scottlogic.librarygradproject.Controllers;
 
+import com.scottlogic.librarygradproject.Entities.Borrow;
 import com.scottlogic.librarygradproject.Services.BorrowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -21,4 +24,12 @@ public class BorrowController {
     @RequestMapping(value = "/borrow/{bookId}", method = RequestMethod.POST)
     public void post(@PathVariable long bookId, OAuth2Authentication authentication) { borrowService.borrow(bookId, authentication); }
 
+    @RequestMapping(value = "/borrow", method = RequestMethod.GET)
+    public List<Borrow> getAll() { return borrowService.findAll(); }
+
+    @RequestMapping(value = "/borrow/{borrowId}", method = RequestMethod.GET)
+    public Borrow get(@PathVariable long borrowId) { return borrowService.get(borrowId); }
+
+    @RequestMapping(value = "/borrow/{borrowId}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable long borrowId) {borrowService.delete(borrowId);}
 }
--- src/main/java/com/scottlogic/librarygradproject/Services/BorrowService.java
+++ src/main/java/com/scottlogic/librarygradproject/Services/BorrowService.java
@@ -1,11 +1,15 @@
 package com.scottlogic.librarygradproject.Services;
 
 import com.scottlogic.librarygradproject.Entities.Borrow;
+import com.scottlogic.librarygradproject.Exceptions.BorrowNotFoundException;
 import com.scottlogic.librarygradproject.Repositories.BorrowRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 public class BorrowService {
 
@@ -23,5 +27,22 @@ public class BorrowService {
         Date date = new Date();
         Borrow loan = Borrow.builder().bookId(bookId).userId(userId).isActive(true).borrowDate(date).build();
         borrowRepository.save(loan);
+    }
+
+    public List<Borrow> findAll() {
+        return borrowRepository.findAll();
+    }
+
+    public Borrow get(long borrowId) {
+        Optional<Borrow> borrowToGet = borrowRepository.findById(borrowId);
+        return borrowToGet.orElseThrow(() -> new BorrowNotFoundException(borrowId));
+    }
+
+    public void delete(long borrowId) {
+        try {
+            borrowRepository.deleteById(borrowId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new BorrowNotFoundException(borrowId);
+        }
     }
 }
