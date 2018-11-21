c2ae9776925414709ab9926c2116cd764e167087
Ronan Graham <Ro4052@users.noreply.github.com>
2018-11-21 14:38:53 +0000

    Change ip from localhost

--- src/main/client/e2eTests/e2e.test.js
+++ src/main/client/e2eTests/e2e.test.js
@@ -12,7 +12,7 @@ beforeAll(async () => {
     //args: ['--disable-dev-shm-usage'],
   });
   page = await browser.newPage();
-  await page.goto("http://localhost:8090/");
+  await page.goto(`http://${require("ip").address()}:8090/`);
   // await page.waitForSelector(styles.pageHeader);
 });
 
