dce0f0be2eb26c765d34277d5f44eb5f5cc896aa
Harry Bedford <hbedford@scottlogic.co.uk>
2018-11-27 12:49:56 +0000

    added backend functionality to retrieve user loans and reservations

--- src/main/java/com/scottlogic/librarygradproject/Services/UserService.java
+++ src/main/java/com/scottlogic/librarygradproject/Services/UserService.java
@@ -1,8 +1,12 @@
 package com.scottlogic.librarygradproject.Services;
 
+import com.scottlogic.librarygradproject.Entities.Borrow;
 import com.scottlogic.librarygradproject.Entities.LibraryUser;
+import com.scottlogic.librarygradproject.Entities.Reservation;
 import com.scottlogic.librarygradproject.Exceptions.UserNotFoundException;
+import com.scottlogic.librarygradproject.Repositories.BorrowRepository;
 import com.scottlogic.librarygradproject.Repositories.LibraryUserRepository;
+import com.scottlogic.librarygradproject.Repositories.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 
@@ -13,10 +17,25 @@ import java.util.Optional;
 public class UserService {
 
     private final LibraryUserRepository userRepo;
+    private final ReservationRepository reservationRepository;
+    private final BorrowRepository borrowRepository;
 
     @Autowired
-    public UserService(LibraryUserRepository userRepo) {
+    public UserService(LibraryUserRepository userRepo, ReservationRepository reservationRepository,
+                       BorrowRepository borrowRepository) {
         this.userRepo = userRepo;
+        this.reservationRepository = reservationRepository;
+        this.borrowRepository = borrowRepository;
+    }
+
+    public LibraryUser getUserDetails(OAuth2Authentication authentication) {
+        Map<String, String> userDetails = (Map<String, String>) authentication.getUserAuthentication().getDetails();
+        LibraryUser newUser = LibraryUser.builder()
+                .userId(userDetails.get("login"))
+                .name(userDetails.get("name"))
+                .avatarUrl(userDetails.get("avatar_url"))
+                .build();
+        return newUser;
     }
 
     public List<LibraryUser> findAll() {
@@ -33,13 +52,17 @@ public class UserService {
     }
 
     public LibraryUser loggedIn(OAuth2Authentication authentication) {
-        Map<String, String> userDetails = (Map<String, String>) authentication.getUserAuthentication().getDetails();
-        LibraryUser newUser = LibraryUser.builder()
-                .userId(userDetails.get("login"))
-                .name(userDetails.get("name"))
-                .avatarUrl(userDetails.get("avatar_url"))
-                .build();
-        return userRepo.save(newUser);
+        return userRepo.save(getUserDetails(authentication));
+    }
+
+    public List<Reservation> findUserReservations(OAuth2Authentication authentication) {
+        String userId = getUserDetails(authentication).getUserId();
+        return reservationRepository.findAllByUserId(userId);
+    }
+
+    public List<Borrow> findUserBorrows(OAuth2Authentication authentication) {
+        String userId = getUserDetails(authentication).getUserId();
+        return borrowRepository.findAllByUserId(userId);
     }
 }
 
