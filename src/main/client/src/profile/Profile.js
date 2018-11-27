7d5863b4ed3793f68e609443d1347387a2f4cdb7
EllieHield <42339621+EllieHield@users.noreply.github.com>
2018-11-27 14:26:27 +0000

    added page header to profile page

--- src/main/client/src/profile/Profile.js
+++ src/main/client/src/profile/Profile.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class Profile extends Component {
+import PageHeader from "../common/pageHeader/PageHeader";
+import styles from "./Profile.module.css";
+
+class Profile extends Component {
   render() {
-    return <div>Profile Page</div>;
+    return (
+      <div className={styles.profile}>
+        <PageHeader user={this.props.user} loggedIn={this.props.loggedIn} />
+        <div className={styles.profileInfoContainer}>Info</div>
+      </div>
+    );
   }
 }
+
+const mapStateToProps = state => ({
+  user: state.login.user,
+  loggedIn: state.login.loggedIn
+});
+
+export default connect(mapStateToProps)(Profile);
