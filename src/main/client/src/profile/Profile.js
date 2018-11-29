dbf1d1da194aebd526e460be9189277fb3df7168
EllieHield <42339621+EllieHield@users.noreply.github.com>
2018-11-29 09:22:15 +0000

    outline of profile page

--- src/main/client/src/profile/Profile.js
+++ src/main/client/src/profile/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Image } from "semantic-ui-react";
 
 import history from "../history";
 import PageHeader from "../common/pageHeader/PageHeader";
@@ -16,7 +17,22 @@ class Profile extends Component {
     return (
       <div className={styles.profile}>
         <PageHeader user={this.props.user} loggedIn={this.props.loggedIn} />
-        <div className={styles.profileInfoContainer}>Info</div>
+        {this.props.user && (
+          <div className={styles.profileInfoContainer}>
+            <Image
+              circular
+              centered
+              size="small"
+              src={this.props.user.avatarUrl}
+            />
+            <h2 className={styles.username}>{this.props.user.name}</h2>
+            <h4 className={styles.username}>
+              user ID: {this.props.user.userId}
+            </h4>
+            <h3>Active Loans:</h3>
+            <h3>Reservations:</h3>
+          </div>
+        )}
       </div>
     );
   }
