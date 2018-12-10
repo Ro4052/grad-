30304ffee9a7a1d56f0af05dbc88cd2205903cf8
Harry Bedford <hbedford@scottlogic.co.uk>
2018-12-10 09:19:41 +0000

    created switch statement within infoText to replace nested ternary

--- src/main/client/src/dashBoard/bookList/book/InfoText.js
+++ src/main/client/src/dashBoard/bookList/book/InfoText.js
@@ -4,6 +4,23 @@ import { Icon, Popup } from "semantic-ui-react";
 const moment = require("moment");
 
 export default class InfoText extends Component {
+  chooseText(book, reservation, borrow) {
+    switch (book.state) {
+      case "Available to Borrow":
+        return `You can borrow this book until ${moment()
+          .add(7, "days")
+          .format("ddd Do MMM YYYY")}`;
+      case "Borrower - Return Not Started":
+        return `You must return this book by ${moment(borrow.returnDate).format(
+          "ddd Do MMM YYYY"
+        )}`;
+      default:
+        return `You must collect this book by ${moment(
+          reservation.collectBy
+        ).format("ddd Do MMM YYYY")}, else your reservation will be cancelled`;
+    }
+  }
+
   render() {
     const { book, borrow, reservation } = this.props;
     return (
@@ -16,21 +33,7 @@ export default class InfoText extends Component {
             position="top center"
             verticalOffset={10}
             trigger={<Icon name="question" />}
-            content={
-              book.state === "Available to Borrow"
-                ? `You can borrow this book until ${moment()
-                    .add(7, "days")
-                    .format("ddd Do MMM YYYY")}`
-                : book.state === "Borrower - Return Not Started"
-                ? `You must return this book by ${moment(
-                    borrow.returnDate
-                  ).format("ddd Do MMM YYYY")}`
-                : `You must collect this book by ${moment(
-                    reservation.collectBy
-                  ).format(
-                    "ddd Do MMM YYYY"
-                  )}, else your reservation will be cancelled`
-            }
+            content={this.chooseText(book, reservation, borrow)}
             hideOnScroll
           />
         )}
